235fa9d249b2249c2bf7c8614482dcf3f318be6a
Chim Kan <designium@gmail.com>
2012-04-27 17:13:41 -0400

    Adding Rails 3.2 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhiteWhale::Application.config.secret_token = 'a447027a23c3ff3b32eff5e0e90ab11aa5926aa0ff10b8d613bcabfc1b255ec6cb32584c502179b008f9639aeb93ef85d2e2c6fbe46ea379d47afc748a3f89b1'
