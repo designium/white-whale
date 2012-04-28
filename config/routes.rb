5ce586f49fb10859314ac26e257966aeebe9c39a
Chim Kan <designium@gmail.com>
2012-04-28 15:59:46 -0400

    Basic testing with Bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ WhiteWhale::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :posts
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -48,7 +50,7 @@ WhiteWhale::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
